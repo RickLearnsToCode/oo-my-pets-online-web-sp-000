6d1225e48d8c41ee44267f6c0816386fc202e69b
Rick Rosetti <rick.rosetti@gmail.com>
2019-05-21 22:03:22 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,34 @@
 class Owner
-  # code goes here
+  attr_reader :species
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(species)
+    @species = species
+  end
+
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@all.length
+  end
+
+  def self.reset_all
+    @@all = []
+  end
+
+  def say_species
+    @species
+  end
+
+
+
+
+
+
+
 end
