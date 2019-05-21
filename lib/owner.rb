2f3c3653853e4c03564216078f02eb8a55d9a3ec
Rick Rosetti <rick.rosetti@gmail.com>
2019-05-21 22:11:47 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -33,12 +33,15 @@ class Owner
 
 
   def buy_fish(name)
-    new_fish = Fish.new(name)
-    @pets[:fishes] << new_fish
+    @pets[:fishes] << Fish.new(name)
   end
 
   def buy_cat(name)
     @pets[:cats] << Cat.new(name)
+  end
+
+  def buy_dog(name)
+    @pets[:dogs] << Dog.new(name)
   end
 
 
